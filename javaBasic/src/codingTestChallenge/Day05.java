3b9d8adc408a12aa88939ae6ec8d02f2eba88d0c
김영서 <dudtj926@naver.com>
2025-01-10 11:29:52 +0900

    코딩테스트 챌린지 Day05
    코딩테스트 챌린지 Day05

---
+++ javaBasic/src/codingTestChallenge/Day05.java
@@ -0,0 +1,28 @@
+package codingTestChallenge;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Day05 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		int[] arr = new int[N];
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		Arrays.sort(arr);
+		
+		int min = arr[0];
+		int max = arr[arr.length-1];
+		
+		System.out.println(min + " " + max);
+
+	}
+}
