28a1ff1a1888663c87e95756a0180a166ca4a70d
김영서 <dudtj926@naver.com>
2025-01-12 12:15:59 +0900

    코딩테스트 Day07
    코딩테스트 Day07

---
+++ javaBasic/src/codingTestChallenge/Day07.java
@@ -0,0 +1,24 @@
+package codingTestChallenge;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Day07 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = br.readLine();
+
+		StringBuilder sb = new StringBuilder(str);
+
+		if (str.equals(sb.reverse().toString())) {
+			System.out.println(1);
+		} else {
+			System.out.println(0);
+		}
+
+	}
+
+}
