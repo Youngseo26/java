144339132d616a0b36c5d02842a9523d45514f3a
김영서 <dudtj926@naver.com>
2025-02-01 23:12:26 +0900

    추상화
    추상화

---
+++ javaBasic/src/ch35/Bird.java
@@ -0,0 +1,39 @@
+package ch35;
+
+public class Bird extends Animal {
+
+	private long weight;
+	private String color;
+
+	public long getWeight() {
+		return weight;
+	}
+
+	public void setWeight(long weight) {
+		this.weight = weight;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	// 행위
+
+	@Override
+	public void eat(String food) {
+		System.out.println("새가 부리로 " + food + "를 먹고있습니다.");
+	}
+
+	@Override
+	public void sleep() {
+		System.out.println("새가 잠을 자고있습니다.");
+	}
+
+	public void fly() {
+		System.out.println("새가 하늘을 날고있습니다.");
+	}
+}
