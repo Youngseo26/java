f1a3f4a9f5065e02bcce2ca439f1447fd5bcb99b
김영서 <dudtj926@naver.com>
2025-03-06 20:10:36 +0900

    코테
    코테

--- javaBasic/src/codingTestChallenge/입문반/Day04.java
+++ javaBasic/src/codingTestChallenge/입문반/Day04.java
@@ -30,18 +30,17 @@ public class Day04 {
 			public int compare(String[] o1, String[] o2) {
 				if(o1[3].equals(o2[3])) {
 					if(o1[2].equals(o2[2])) {
-						return Integer.parseInt(o1[1]) - Integer.parseInt(o2[1]);
+						return Integer.parseInt(o1[1]) - Integer.parseInt(o2[1]); // 태어난 월이 같으면 태어난 일로 정렬
 					}
-					return Integer.parseInt(o1[2]) - Integer.parseInt(o2[2]);
+					return Integer.parseInt(o1[2]) - Integer.parseInt(o2[2]); //태어난 해가 같으면 태어난 월로 정렬
 				}
-				return Integer.parseInt(o1[3]) - Integer.parseInt(o2[3]);
+				return Integer.parseInt(o1[3]) - Integer.parseInt(o2[3]); // 태어난 해 정렬
 			}			
 		});
 		
 		StringBuilder sb = new StringBuilder();
-		sb.append(arr[n-1][0]).append("\n").append(arr[0][0]);
+		sb.append(arr[n-1][0]).append("\n").append(arr[0][0]); // 맨 뒤에 있는 사람이 가장 어린 사람, 맨 앞에 있는 사람이 가장 나이 많은 사람
 		System.out.print(sb);
 
 	}
-
 }
