b3645bbb2fea52d1a39bf11bd79da222349b8b07
김영서 <dudtj926@naver.com>
2025-03-08 20:59:01 +0900

    Day06
    Day06

---
+++ javaBasic/src/codingTestChallenge/입문반/Day06.java
@@ -0,0 +1,43 @@
+package codingTestChallenge.입문반;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Day06 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		StringBuilder sb  = new StringBuilder();
+		int[] arr = new int[5];
+		int temp = 0;
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = 0; j < arr.length - 1 - i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+					for(int x = 0; x < arr.length; x++) {
+						sb.append(arr[x] + " ");
+					}
+					sb.append("\n");
+				}
+			}
+		}
+		bw.write(sb + "");
+		bw.flush();
+		bw.close();
+	}
+
+}
